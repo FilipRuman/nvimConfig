847726d3f894daed825c4900146c5acfa747b90d
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-05 12:49:52 +0100

    moved more lazy packages to separate files + changed cmp keymaps

---
+++ lua/custom/plugins/lazyDev.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    -- `lazydev` configures lua lsp for your neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+}
