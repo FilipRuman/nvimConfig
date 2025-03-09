7d3ca0ad40f7a2eb8e0f14463f3aef0734b79021
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-09 15:41:06 +0100

    added neovide config

--- init.lua
+++ init.lua
@@ -26,6 +26,17 @@ vim.g.rustaceanvim = {
   },
 }
 
+-- neovide
+vim.g.neovide_fullscreen = true
+vim.g.neovide_cursor_vfx_mode = 'pixiedust'
+vim.g.neovide_refresh_rate = 144
+vim.o.guifont = 'MesloLGM Nerd Font:h14'
+if vim.g.neovide == true then
+  vim.api.nvim_set_keymap('n', '<C-=>', ':lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  1.0)<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<C-->', ':lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<C-+>', ':lua vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + 0.05, 1.0)<CR>', { silent = true })
+  vim.api.nvim_set_keymap('n', '<C-_>', ':lua vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - 0.05, 0.0)<CR>', { silent = true })
+end
 -- spell
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
