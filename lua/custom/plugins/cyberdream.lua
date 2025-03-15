34b99fd7db8bc3e7e9272f01e40c4e463a2474f3
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-15 22:05:34 +0100

    moved back to cyber dream

--- lua/custom/plugins/cyberdream.lua
+++ lua/custom/plugins/cyberdream.lua
@@ -1,11 +1,19 @@
 return {
-  -- {
-  --   'scottmckendry/cyberdream.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('cyberdream').setup {}
-  --     vim.cmd 'colorscheme cyberdream'
-  --   end,
-  -- },
+  {
+    'scottmckendry/cyberdream.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('cyberdream').setup {
+
+        highlights = {
+          -- Highlight groups to override, adding new groups is also possible
+          -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+          Comment = { fg = '#a1c639', italic = true },
+        },
+      }
+      vim.cmd 'colorscheme cyberdream'
+    end,
+  },
 }
