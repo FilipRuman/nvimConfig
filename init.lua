452a59980dac115f69c073f7149a94120fdc36e5
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-16 08:32:05 +0100

    added signs for todo comments

--- init.lua
+++ init.lua
@@ -168,8 +168,9 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
+  --https://github.com/folke/todo-comments.nvim
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
+  --PERF:
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
